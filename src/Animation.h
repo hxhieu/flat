001bc5787e84bd5dd5ad4096685de492d9a09f56
Hugh Hoang <hugh.hoang@gmail.com>
2020-05-14 20:36:58 +1200

    feat: allow run once animation i.e. not looping

--- src/Animation.h
+++ src/Animation.h
@@ -24,9 +24,10 @@ namespace flat2d
 			Clips clips;
 			uint32_t timestep;
 			int clipIndex = 0;
+            		bool runOnce = false;
 
 		public:
-			Animation(Clips &c, uint32_t t) : clips(c), timestep(t) { }
+			Animation(Clips &c, uint32_t t, bool once = false) : clips(c), timestep(t), runOnce(once) {}
 
 			/**
 			 * Run the animation and get the current clip
@@ -44,11 +45,23 @@ namespace flat2d
 			 */
 			void stop();
 
-			/**
+        /**
 			 * Check if the animation is running
 			 * @return True or false indicating if the animation is running
 			 */
 			bool isRunning() const;
+
+            		/**
+             		 * Reset the animation if it was set to run once 
+             		 * @param alsoStart start the animation after reset
+             		 */
+            		void reset(bool alsoStart = true);
+
+            		/**
+             		 * Set the animation to run once i.e not looping
+             		 * @param once True to stop the animation from looping
+             		 */
+            		void setRunOnce(bool once);
 	};
 } // namespace flat2d
 
